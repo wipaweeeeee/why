228fc9bce62ca0c1f1456c8af8f11a27470ca57e
wipaweeeeee <wsirikolkarn@gmail.com>
2018-04-11 01:37:02 -0400

    ejs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'),
 	  path = require('path'),
       app = express();
 
+app.set('view engine', 'ejs')
+
 app.set('port', (process.env.PORT || 8080));
 
 //app.use(express.static('public'));
@@ -9,17 +11,17 @@ app.set('port', (process.env.PORT || 8080));
 var TextToSVG = require('text-to-svg');
 var textToSVG = TextToSVG.loadSync();
  
-var attributes = {fill: 'red', stroke: 'black'};
+var attributes = {id: 'h'};
 var options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
  
-var svg = textToSVG.getSVG('hello', options);
+var svg = textToSVG.getSVG('h', options);
  
 console.log(svg);
 
 app.get('/', function(req,res) {
 	
-	res.sendFile(path.join(__dirname+'/index.html'));
-	//res.send(svg);
+	//res.sendFile(path.join(__dirname+'/index.html'));
+	res.render('index', {svg: svg});
 })
 
 app.listen(app.get('port'), function(err) {
